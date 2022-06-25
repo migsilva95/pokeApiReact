f4785514a6dbbe0754ba3bb7d218ecb4da267e58
unknown <miguelflsilva@hotmail.com>
2022-06-25 19:29:12 +0100

    Updated beauty of the homepage

--- src/App.js
+++ src/App.js
@@ -3,25 +3,29 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import HomePage from './components/HomePage';
 import Info from './components/Info';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
   const [selectedPokemon, setSelectedPokemon] = useState({});
   const [showHomePage, setShowHomePage] = useState(false);
   const [showInfo, setShowInfo] = useState(false);
+  const [first, setFirst] = useState(0);
+  const [last, setLast] = useState(17);
   
   useEffect(() => {
     if(localStorage.getItem("pokemons") === null){
       fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
         .then((response) => response.json())
         .then((data) => {
-          const pokemons = data.results.map(obj => {
-            return {...obj, favorite: false, detail: {}}
+          const pokemons = data.results.map((obj, index) => {
+            return {...obj, favorite: false, id: index, number: obj.url.slice(34, -1) }
           })
           localStorage.setItem("pokemons", JSON.stringify(pokemons));
         })
       }
       setShowHomePage(true);
+      setShowInfo(false);
   },[])
 
   const pokeInfo = (name) => {
@@ -41,7 +45,7 @@ function App() {
 
   return (
     <div className="App">
-      {showHomePage && <HomePage pokeInfo={pokeInfo} />}
+      {showHomePage && <HomePage pokeInfo={pokeInfo} first={first} last={last} setFirst={setFirst} setLast={setLast} />}
       {showInfo && <Info selectedPokemon={selectedPokemon} backFunction={backFunction} />}
     </div>
   );
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,7 @@
 import './../App.css';
 import { useState, useEffect } from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Row, Button, Col } from 'react-bootstrap';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function HomePage(props) {
 
@@ -8,26 +9,46 @@ function HomePage(props) {
   const pokemons = JSON.parse(window.localStorage.getItem('pokemons'));
 
   pokemons && pokemons.forEach((pokemon, index) => {
-    items.push(
-        <Card
-          tag="a"
-          border="dark"
-          style={{ width: '18rem', cursor: 'pointer' }}
-          onClick={() => props.pokeInfo(pokemon.name)}
-        >
-          <Card.Header>{pokemon.name}</Card.Header>
-          <Card.Body>
-            <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + (index + 1) + ".png"} />
-          </Card.Body>
-        </Card>
-    )
-  })
+    if (index >= props.first && index <= props.last) {
+      items.push(
+        <Col>
+          <Card
+            tag="a"
+            border="dark"
+            style={{ cursor: 'pointer' }}
+            onClick={() => props.pokeInfo(pokemon.name)}
+          >
+            <Card.Header>{pokemon.name}</Card.Header>
+            <Card.Body>
+              <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + pokemon.number + ".png"} />
+            </Card.Body>
+          </Card>
+        </Col>
+      )
+    }
+  });
 
+  const next = () => {
+    props.setFirst(props.first + 18);
+    props.setLast(props.last + 18);
+  };
+
+  const previous = () => {
+    props.setFirst(props.first - 18);
+    props.setLast(props.last - 18);
+  };
 
   return (
-    <div className="list-group">
-      {items}
-    </div>
+    <>
+      <Row xs={1} sm={2} md={6} className="g-2">
+        {items}
+      </Row>
+      <br/>
+      <Row>
+        {props.first > 0 && <div style={{ display: "flex", flex: 1, justifyContent: "flex-start" }}><Button variant="secondary" onClick={() => previous()} >Previous</Button></div>}
+        {props.first <= pokemons.length - 18 && <div style={{ display: "flex", flex: 1, justifyContent: "flex-end" }}><Button variant="secondary" onClick={() => next()} >Next</Button></div>}
+      </Row>
+    </>
   );
 }
 
